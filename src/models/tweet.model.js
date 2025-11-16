7604f2a51a07dd9867748e03c6267fdd4b9f436d
dhruvsoni461 <dhruvkumarsoni461@gmail.com>
2025-11-16 18:06:14 +0530

    feat: implement comment, like, playlist, and tweet models with mongoose schemas

---
+++ src/models/tweet.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
+},
+{               
+    timestamps: true,
+})
+
+export default mongoose.model("Tweet", tweetSchema);
